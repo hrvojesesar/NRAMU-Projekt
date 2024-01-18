fe22fbe415811dccd25064a4654277fb96589f60
AntonioSego <segoantonio22@gmail.com>
2024-01-18 20:20:37 +0100

    Created List to Note Firebase

--- app/src/main/java/ba/sum/fsre/nramu_projekt/Utility.java
+++ app/src/main/java/ba/sum/fsre/nramu_projekt/Utility.java
@@ -4,10 +4,13 @@ import android.content.Context;
 import android.widget.Toast;
 
 import com.google.firebase.Firebase;
+import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.SimpleDateFormat;
 
 public class Utility {
 
@@ -18,5 +21,9 @@ public class Utility {
     static CollectionReference getCollectionReferenceForNotes() {
         FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
        return FirebaseFirestore.getInstance().collection("notes").document(currentUser.getUid()).collection("my_notes");
+    }
+
+    static String timestampToString(Timestamp timestamp){
+       return new SimpleDateFormat("MM/dd/yyyy").format(timestamp.toDate());
     }
 }
